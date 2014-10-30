87b37e238febf5e5bb39a0e9737f20dc93eded41
CJ <thecjreynolds@gmail.com>
2014-10-29 20:50:12 -0400

    Initial commit

---
+++ instructions.js
@@ -0,0 +1,44 @@
+﻿{
+	"instructions": [{
+		"name": "Left Motor and Arm",
+		"total": 0,
+		"children": [{
+				"name": "Left Arm",
+				"total": 7
+			},{
+				"name": "Left Motor",
+				"total": 4
+			},{
+				"name": "Combine Left Motor and Left Arm",
+				"total": 3
+			}]
+	},{
+		"name": "Right Motor and Arm",
+		"total": 0,
+		"children": [{
+				"name": "Right Arm",
+				"total": 7
+			},{
+				"name": "Right Motor",
+				"total": 5
+			},{
+				"name": "Combine Right Motor and Right Arm",
+				"total": 3
+			}]
+	},{
+		"name": "Combine Motors",
+		"total": 5
+	},{
+		"name": "Back Wheel",
+		"total": 6
+	},{
+		"name": "Attach Brick",
+		"total": 5
+	},{
+		"name": "Attach Sensor",
+		"total": 3
+	},{
+		"name": "Connect Motors and Sensor",
+		"total": 4
+	}]
+}
