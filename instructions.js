93f0f7c272e7ce0c53f94e093ee38657517ebf8c
CJ <atticusalien.x@gmail.com>
2014-11-12 08:41:27 -0500

    partially assembled

--- instructions.js
+++ instructions.js
@@ -1,36 +1,10 @@
 ﻿{
 	"instructions": [{
-		"name": "Left Motor and Arm",
-		"total": 0,
-		"children": [{
-				"name": "Left Arm",
-				"total": 7
-			},{
-				"name": "Left Motor",
-				"total": 4
-			},{
-				"name": "Combine Left Motor and Left Arm",
-				"total": 3
-			}]
-	},{
-		"name": "Right Motor and Arm",
-		"total": 0,
-		"children": [{
-				"name": "Right Arm",
-				"total": 7
-			},{
-				"name": "Right Motor",
-				"total": 5
-			},{
-				"name": "Combine Right Motor and Right Arm",
-				"total": 3
-			}]
-	},{
 		"name": "Combine Motors",
 		"total": 5
 	},{
 		"name": "Back Wheel",
-		"total": 6
+		"total": 1
 	},{
 		"name": "Attach Brick",
 		"total": 5
