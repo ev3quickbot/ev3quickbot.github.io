9dfe67a7f836945ed890a93a6409044c293e66d2
atticusalien <atticusalienx@gmail.com>
2015-09-05 10:40:11 -0600

    Re-vert to full instructions

--- instructions.js
+++ instructions.js
@@ -1,10 +1,36 @@
-﻿{
+{
 	"instructions": [{
+		"name": "Left Motor and Arm",
+		"total": 0,
+		"children": [{
+				"name": "Left Arm",
+				"total": 7
+			},{
+				"name": "Left Motor",
+				"total": 4
+			},{
+				"name": "Combine Left Motor and Left Arm",
+				"total": 3
+			}]
+	},{
+		"name": "Right Motor and Arm",
+		"total": 0,
+		"children": [{
+				"name": "Right Arm",
+				"total": 7
+			},{
+				"name": "Right Motor",
+				"total": 5
+			},{
+				"name": "Combine Right Motor and Right Arm",
+				"total": 3
+			}]
+	},{
 		"name": "Combine Motors",
 		"total": 5
 	},{
 		"name": "Back Wheel",
-		"total": 1
+		"total": 6
 	},{
 		"name": "Attach Brick",
 		"total": 5
